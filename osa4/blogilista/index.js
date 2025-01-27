c0dcf81f73ac9f8fabba961f3ad680a7e5edb38e
unknown <iph.viva@gmail.com>
2025-01-27 13:43:17 +0200

    Fixed function import

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -3,5 +3,5 @@ const { PORT } = require("./utils/config");
 const logger = require("./utils/logger");
 
 app.listen(PORT, () => {
-	logger.log(`Server running on port ${PORT}`);
+	logger.info(`Server running on port ${PORT}`);
 });
