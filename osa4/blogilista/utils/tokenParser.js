9e87efbd2f49c2435c2edc18c516a339f057a402
Ilkka_hay <iph.viva@gmail.com>
2025-02-03 14:49:29 +0200

    Remove debug console logs from token parser middleware

--- osa4/blogilista/utils/tokenParser.js
+++ osa4/blogilista/utils/tokenParser.js
@@ -1,14 +1,10 @@
 export const tokenParser = (request, response, next) => {
-	console.log("1");
 	const authorization = request.get("authorization");
 	if (authorization?.startsWith("Bearer ")) {
-		console.log("2");
 		request.token = authorization.replace("Bearer ", "");
 	} else {
-		console.log("3");
 		request.token = null;
 	}
 
-	console.log("4");
 	next();
 };
