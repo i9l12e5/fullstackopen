eeb87f77e74a65edc447f072e8238e28d42165a4
unknown <iph.viva@gmail.com>
2025-02-10 16:53:56 +0200

    Task 5.7

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,25 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+	const [inView, setInView] = useState(false);
+
+	return (
+		<div style={{ border: "solid black 1px", padding: "3px" }}>
+			{blog.title} {blog.author}{" "}
+			<button type="button" onClick={() => setInView(!inView)}>
+				view
+			</button>
+			{inView ? (
+				<div>
+					<div>{blog.url}</div>
+					<div>
+						likes {blog.likes} <button type="button">like</button>
+					</div>
+					<div>{blog.user.username}</div>
+				</div>
+			) : null}
+		</div>
+	);
+};
+
+export default Blog;
